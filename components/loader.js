ec7752361a5b02fb3faec6ed58640c650d8be062
Matthew Murphy <matt@mattmurphy.ca>
2022-01-19 08:06:05 -0800

    fix spinner

--- components/loader.js
+++ components/loader.js
@@ -1,7 +1,7 @@
 export const Loader = () => (
 	<>
-		<div className='loadingio-spinner-dual-ball-m62qwe4ybl'>
-			<div className='ldio-thrnr52wyj'>
+		<div className='loadingio-spinner-dual-ball-mk7f3bd4cy'>
+			<div className='ldio-57x5d5q6m4j'>
 				<div></div>
 				<div></div>
 				<div></div>
